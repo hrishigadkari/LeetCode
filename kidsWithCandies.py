44b6467502048656134c752c722d46837d790336
hrishigadkari <59075423+hrishigadkari@users.noreply.github.com>
2020-06-13 17:57:54 -0400

    Add files via upload

---
+++ kidsWithCandies.py
@@ -0,0 +1,4 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        return [(candies[i] + extraCandies) >= max(candies) for i in range(0,len(candies))]
+            
