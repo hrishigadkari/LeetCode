2eb68f643199daaa194f707c03ddd068868d66fb
hrishigadkari <59075423+hrishigadkari@users.noreply.github.com>
2020-06-13 18:15:24 -0400

    Delete kidsWithCandies.py

--- kidsWithCandies.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        return [(candies[i] + extraCandies) >= max(candies) for i in range(0,len(candies))]
-            
